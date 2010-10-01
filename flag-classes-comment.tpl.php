bca9961c126ef85ac03a6371305980b2b16ba15d
Mooffie <mooffie@gmail.com>
2010-10-01 11:17:26 +0200

    Wrap comments and users as well.

---
+++ flag-classes-comment.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * A wrapper around comment.tpl.php.
+ */
+?>
+<div class="flag-classes-wrapper flag-classes-comment-wrapper <?php echo $flag_classes; ?>">
+  <?php echo $output; ?>
+</div>
---
+++ flag-classes-user-profile.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * A wrapper around user-profile.tpl.php.
+ */
+?>
+<div class="flag-classes-wrapper flag-classes-user-wrapper <?php echo $flag_classes; ?>">
+  <?php echo $output; ?>
+</div>
